9ede64bc823297e9d65eb1d7ad46fe2d0b12baf6
seong su kim <know.warehouse02@gmail.com>
2025-04-29 17:48:14 +0900

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import pageStyle from '../app/styles/page.module.css';
 
 export default function Homepage() {
   return (
     <>
-      <div className="container">
+      <div className={pageStyle.container}>
         <Image src="/favicon.png" alt="logo" width="60" height="60" />
-        <h1 className="title">포트원 결제</h1>
-        <p className="sub_title">포트원 결제 샘플입니다.</p>
+        <h1 className={pageStyle.title}>포트원 결제</h1>
+        <p className={pageStyle.sub_title}>포트원 결제 샘플입니다.</p>
         <button>
           <Link href="/ui">결제하러 가볼까요?</Link>
         </button>
